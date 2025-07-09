c428d9daf5bfe4ddea354002e90190682f793159
Samson Nwokike <nwokikesamson@gmail.com>
2025-07-09 23:52:19 +0100

    ch: clean up

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,20 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\EventRequest;
 use App\Models\Event;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class EventController extends Controller
 {
-    function index()
+    public function index()
     {
         $events = Event::all();
+
         return Inertia::render('home', [
-            'events' => $events
+            'events' => $events,
         ]);
     }
 
-    function store(EventRequest $request)
+    public function store(EventRequest $request)
     {
         try {
             $event = Event::create($request->validated());
