35f5f82d9f4eb65be9ef6bbd519b70a79735b3aa
Samson Nwokike <nwokikesamson@gmail.com>
2025-07-09 23:13:14 +0100

    feat: improved types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,43 +1,15 @@
-import { LucideIcon } from 'lucide-react';
-import type { Config } from 'ziggy-js';
-
-export interface Auth {
-    user: User;
+export interface CalendarEventsProps {
+    events: CalendarEvent[];
 }
 
-export interface BreadcrumbItem {
-    title: string;
-    href: string;
-}
-
-export interface NavGroup {
-    title: string;
-    items: NavItem[];
-}
-
-export interface NavItem {
-    title: string;
-    href: string;
-    icon?: LucideIcon | null;
-    isActive?: boolean;
-}
-
-export interface SharedData {
+export type CalendarEvent = {
+    id?: string;
     name: string;
-    quote: { message: string; author: string };
-    auth: Auth;
-    ziggy: Config & { location: string };
-    sidebarOpen: boolean;
-    [key: string]: unknown;
-}
-
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-    avatar?: string;
-    email_verified_at: string | null;
-    created_at: string;
-    updated_at: string;
-    [key: string]: unknown; // This allows for additional properties...
-}
+    start_date: string;
+    end_date: string;
+    start_time: string;
+    end_time: string;
+    recurrent: boolean;
+    recurrent_type: 'daily' | 'weekly' | 'monthly';
+    include_weekends: boolean;
+};
